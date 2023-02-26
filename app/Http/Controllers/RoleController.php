0ff91c72379f1996c91edda21d631e9167787a7e
fatimatuzzahroe <fatimatuz.zahroe@gmail.com>
2023-02-26 09:49:53 +0700

    fungsi delete

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
 use Illuminate\Http\Request;
+use App\Http\Requests\RoleRequest;
 use App\Http\Requests\ProfileUpdateRequest;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 use Illuminate\Support\Facades\Gate;
+use App\DataTables\RoleDataTable;
+
 
 
 class RoleController extends Controller
@@ -21,13 +25,15 @@ class RoleController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(Request $request)
+    public function index(RoleDataTable $dataTable)
     {
       $this->authorize('read role');
+      return $dataTable->render('konfigurasi.role');
+
     //    if(Gate::allows('read role')){
     //     abort(403,'unautorized');
     //    }
-        return view('konfigurasi.role');
+       // return view('konfigurasi.role');
     }
 
     /**
@@ -57,24 +63,36 @@ class RoleController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Role $role)
     {
-        //
+        return view('konfigurasi.role-action',compact('role'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(RoleRequest $request, Role $role)
     {
-        //
+        $role->name = $request->name;
+        $role->guard_name = $request->guard_name;
+        $role->save();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Update data successfully'
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Role $role)
     {
-        //
+        $role->delete();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Delete data Successfully'
+
+        ]);
     }
 }
--- resources/views/konfigurasi/role.blade.php
+++ resources/views/konfigurasi/role.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.master')
+@push('css')
+<link href="{{ asset('')}}vendor/datatables.net-dt/css/jquery.dataTables.min.css" rel="stylesheet" />
+<link href="{{ asset('')}}vendor/datatables.net-responsive-dt/css/responsive.dataTables.min.css" rel="stylesheet" />
+    
+@endpush
 @section('content')
 <div class="main-content">
     <div class="title">
@@ -12,40 +17,114 @@
                         <h4>Roles</h4>
                     </div>
                     <div class="card-body">
-                        <canvas id="myChart" height="642" width="1388"></canvas>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h4>Statistics</h4>
-                    </div>
-                    <div class="card-body">
-                        <div class="progress-wrapper">
-                            <h4>Progress 25%</h4>
-                            <div class="progress progress-bar-small">
-                                <div class="progress-bar progress-bar-small" style="width: 25%" role="progressbar"
-                                    aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">
-                                </div>
-                            </div>
-                        </div>
-                        <div class="progress-wrapper">
-                            <h4>Progress 45%</h4>
-                            <div class="progress progress-bar-small">
-                                <div class="progress-bar progress-bar-small bg-pink" style="width: 45%"
-                                    role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100">
-                                </div>
-                            </div>
-                        </div>
-                        <canvas id="myChart2" height="842" width="1388"></canvas>
-
+                        @if (request()->user()->can('create role'))
+                        <button type="button" class="btn btn-primary mb-3">Tambah Data</button>
+                            
+                        @endif
+                        {{ $dataTable->table() }}
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    
-
+    <div class="modal fade" id="modalAction" tabindex="-1" aria-labelledby="largeModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        
+    </div>
+</div>
 </div>
 @endsection
+@push('js')
+<script src="{{ asset('')}}vendor/jquery/dist/jquery.min.js"></script>
+<script src="{{ asset('')}}vendor/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="{{ asset('')}}vendor/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+<script src="{{ asset('')}}vendor/sweetalert2/dist/sweetalert2.all.min.js"></script>
+{{ $dataTable->scripts() }}
+
+<script>
+    const modal = new bootstrap.Modal($('#modalAction'))
+    $('#role-table').on('click', '.action', function(){
+        let data = $(this).data()
+        let id = data.id
+        let jenis = data.jenis
+
+        if(jenis == 'delete'){
+            console.log('delete');
+            Swal.fire({
+                        title: 'Are you sure?',
+                        text: "You won't be able to revert this!",
+                        icon: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'Yes, delete it!'
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            $.ajax({
+                                method: 'DELETE',
+                                url: `{{ url('konfigurasi/roles/') }}/${id}`,
+                                headers: {
+                                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                                        },
+                                success: function(res){
+                                    window.LaravelDataTables["role-table"].ajax.reload()
+                                    Swal.fire(
+                                                'Deleted!',
+                                                res.message,
+                                                res.status
+                                            )
+                                }
+                                
+                             })
+                        }                     
+                    })
+                return
+
+         }
+
+        $.ajax({
+                method: 'get',
+                url: `{{ url('konfigurasi/roles/') }}/${id}/edit`,
+                success: function(res){
+                    $('#modalAction').find('.modal-dialog').html(res)
+                    modal.show()
+                    store()
+                }
+        })
+        function store(){
+            $('#formAction').on('submit', function(e){
+                e.preventDefault()
+                const _form = this
+                const formData = new FormData(_form)
+
+                $.ajax({
+                        method: 'POST',
+                        url: `{{ url('konfigurasi/roles/') }}/${id}`,
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                        },
+                        data: formData,
+                        processData: false,
+                        contentType: false,
+                        success: function(res){
+                            window.LaravelDataTables["role-table"].ajax.reload()
+                            modal.hide()
+                        },
+                        error: function(res){
+                            let errors = res.responseJSON?.errors
+                            $(_form).find('.text-danger.text-small').remove()
+                            if(errors){
+                                for(const [key, value] of Object.entries(errors)){
+                                    $(`[name='${key}']`).parent().append(`<span class="text-danger text-small">${value}</span>`)
+                                }
+                            }
+                        }    
+                })
+            })
+        }
+     })
+    
+</script>
+    
+@endpush
